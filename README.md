59a7dda23864333a65645b3d648f029ea3bf6446
Soluna <julia3.williamson@gmail.com>
2025-01-08 13:38:46 -0300

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Soluna
+## Description
+Library reserved for future use.
+## License
+ARR for now as project future-proofing. License subject to change. 
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, world! I'm Soluna, a Rust game dev working on the Analemma project!");
+    println!("It is a mini-voxel survival game and physics sim.");
+    println!("This is a repository I am maintaining for future use, perhaps as a library of my projects.")
+}
